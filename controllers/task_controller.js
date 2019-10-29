839c3c0a9a7b63e69326bbcd52b2172a44e0682c
anubhuti-sh <anubhutiv@sensehawk.com>
2019-10-29 13:03:16 +0530

    adds async await

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -1,21 +1,33 @@
 const Desc = require('../models/desc');
 
 
-module.exports.addTask = function (req,res){
+module.exports.addTask = async function (req,res){
+    
+    let newDesc;
+    try {
+        newDesc = await Desc.create({
+            desc:req.body.desc,
+            dateOfTask:req.body.dateOfTask,
+            category:req.body.category
+        });
+    } catch(err) {
+        console.log(err);
+    }
 
+    return res.redirect('back');
 
-    Desc.create({
-        desc:req.body.desc,
-        dateOfTask:req.body.dateOfTask,
-        category:req.body.category
-    },function(err,newDesc){
-        if(err){
-            console.log('error in creating new item');
-            return;
-        }
-        console.log('******',newDesc);
-        return res.redirect('back');
-    });
+    // Desc.create({
+    //     desc:req.body.desc,
+    //     dateOfTask:req.body.dateOfTask,
+    //     category:req.body.category
+    // },function(err,newDesc){
+    //     if(err){
+    //         console.log('error in creating new item');
+    //         return;
+    //     }
+    //     console.log('******',newDesc);
+    //     return res.redirect('back');
+    // });
 
    
 
